6320b41a6eac35d108b8009fb8d48f89557867ff
hongc <hongc@tbi-worker.inet.dkfz-heidelberg.de>
2017-12-08 16:23:12 +0100

    origin version

---
+++ R/mas.R
@@ -0,0 +1,13 @@
+library("devtools")
+#library(roxygen2)
+#setwd("/home/hongc/mas")
+#create("chen")
+
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
